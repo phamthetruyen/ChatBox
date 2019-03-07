a976900394a4aa23d31fe0b401cc309d17858e7a
ASUS <phamthetruyen@hotmail.com>
2019-03-07 08:36:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatbox/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.chatbox;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        Sprite doubleBounce = new DoubleBounce();
+        progressBar.setIndeterminateDrawable(doubleBounce);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =  new Intent(MainActivity.this,Splash.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/example/chatbox/Splash.java
@@ -0,0 +1,79 @@
+package com.example.chatbox;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.internal.service.Common;
+import com.google.android.gms.signin.SignIn;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Splash extends AppCompatActivity {
+    EditText edtPhone,edtPassword;
+    Button btnSingIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        edtPhone = (EditText) findViewById(R.id.edtPhone);
+        edtPassword = (EditText) findViewById(R.id.edtPass);
+        btnSingIn = (Button) findViewById(R.id.btnSignIn);
+
+        // Khai báo Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ProgressDialog mDialog = new ProgressDialog(Splash.this);
+                mDialog.setMessage("Waiting!");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //Kiểm tra xem user tồn tại k
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+
+                            mDialog.dismiss();
+                            // Lấy thông tin user
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            user.setPhone(edtPhone.getText().toString());//set Phone
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                Toast.makeText(Splash.this, "OK", Toast.LENGTH_SHORT).show();
+//                                Intent homeIntent = new Intent(Splash.this,Home.class);
+//                                Common.currentUser = user;
+//                                startActivity(homeIntent);
+//                                finish();
+
+                            } else Toast.makeText(Splash.this, "Fall", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            mDialog.dismiss();
+                            Toast.makeText(Splash.this, "User do not exist", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
